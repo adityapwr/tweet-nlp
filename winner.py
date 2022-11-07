13f2a0b0d1699216d03f501a8a59439ac16b66a9
Aditya Pawar <pawar.aditya29@gmail.com>
2022-11-07 15:44:39 +0530

    feat: Latest changes

--- winner.py
+++ winner.py
@@ -1,9 +1,26 @@
 import re
 import spacy
 import json
+import requests
+from bs4 import BeautifulSoup
 
-### find winners in the event and check with imdb database if the actor exists.
+# find winners in the event and check with imdb database if the actor exists.
 nlp = spacy.load("en_core_web_sm")
+
+
+def check_actor(org_name):
+    org_name = org_name.lower().strip()
+    search_name = org_name.replace(" ", "+")
+    result = requests.get(
+        f"https://www.imdb.com/search/name/?name={search_name}")
+    soup = BeautifulSoup(result.text)
+    all_models = soup.find_all("div", {"class": "lister-item mode-detail"})
+    if len(all_models) > 0:
+        for single_model in all_models:
+            name = single_model.find_all("a")[1].text.lower().strip()
+            if name == org_name:
+                return True
+    return False
 
 
 with open("all_data.txt", "r") as f:
@@ -22,72 +39,79 @@ with open("all_data.txt", "r") as f:
                 doc = nlp(winner.group(6))
                 for ent in doc.ents:
                     if ent.label_ == "PERSON":
-                        #if ent.text not in winners:
-                        winners[ent.text] = winners.get(ent.text, 0) + 1
-                        data[award] = data.get(award, {})
+                        win = ent.text
+                        # if ent.text not in winners:
+                        winners[win] = winners.get(win, 0) + 1
+                        data[win] = data.get(win, {})
                         if category:
-                            data[award][category] = data[award].get(category, {})
-                            data[award][category][ent.text] = data[award][category].get(ent.text, 0) + 1
+                            data[win][award] = data[win].get(award, {})
+                            data[win][award][category] = data[win][award].get(
+                                category, 0) + 1
                         else:
-                            data[award][ent.text] = data[award].get(ent.text, 0) + 1
+                            data[win][award] = data[win].get(award, 0) + 1
             winner_regex_2 = r'(Best.+?)(,|in a)(.*) (is|are) (.*)'
             winner = re.search(winner_regex_2, line)
             if winner:
-                award = winner.group(1).strip()
-                category = winner.group(3).strip()
+                award = winner.group(1).strip().lower()
+                category = winner.group(3).strip().lower()
                 doc = nlp(winner.group(5))
                 for ent in doc.ents:
                     if ent.label_ == "PERSON":
-                        #if ent.text not in winners:
-                        winners[ent.text] = winners.get(ent.text, 0) + 1
-                        data[award] = data.get(award, {})
+                        # if ent.text not in winners:
+                        win = ent.text
+                        # if ent.text not in winners:
+                        winners[win] = winners.get(win, 0) + 1
+                        data[win] = data.get(win, {})
                         if category:
-                            data[award][category] = data[award].get(
-                                category, {})
-                            data[award][category][ent.text] = data[award][category].get(
-                                ent.text, 0) + 1
+                            data[win][award] = data[win].get(award, {})
+                            data[win][award][category] = data[win][award].get(
+                                category, 0) + 1
                         else:
-                            data[award][ent.text] = data[award].get(
-                                ent.text, 0) + 1
-            winner_regex_3 = r'(winner|winners) (is|are) (.*)'
-            winner = re.search(winner_regex_2, line)
+                            data[win][award] = data[win].get(award, 0) + 1
+            winner_regex_3 = r'([B|b]est.*)(,|in a)(.*) [G|g]oes to ([A-Za-z]* [A-Za-z]*)'
+            winner = re.search(winner_regex_3, line)
             if winner:
-                # award = winner.group(1).strip()
-                # category = winner.group(3).strip()
-                # doc = nlp(winner.group(5))
+                award = winner.group(1).strip().lower()
+                category = winner.group(3).strip().lower()
+                doc = nlp(winner.group(4))
                 for ent in doc.ents:
                     if ent.label_ == "PERSON":
                         # if ent.text not in winners:
-                        winners[ent.text] = winners.get(ent.text, 0) + 1
-                        # data[award] = data.get(award, {})
-                        # if category:
-                        #     data[award][category] = data[award].get(
-                        #         category, {})
-                        #     data[award][category][ent.text] = data[award][category].get(
-                        #         ent.text, 0) + 1
-                        # else:
-                        #     data[award][ent.text] = data[award].get(
-                        #         ent.text, 0) + 1
+                        win = ent.text
+                        # if ent.text not in winners:
+                        winners[win] = winners.get(win, 0) + 1
+                        data[win] = data.get(win, {})
+                        if category:
+                            data[win][award] = data[win].get(award, {})
+                            data[win][award][category] = data[win][award].get(
+                                category, 0) + 1
+                        else:
+                            data[win][award] = data[win].get(award, 0) + 1
+        final_winners = {}
         json.dump(data, w, indent=4)
+        winners = {key: val for key, val in winners.items() if val > 2}
+        for k, v in winners.items():
+            # check if person exists from imdb url
+            print(f'Checking {k}')
+            if check_actor(k):
+                print(f'Found {k}')
+                final_winners[k] = v
 
-        print(json.dumps(winners, indent=4))
-        # nominees_list = {}
-        # with open("nominees.txt", "r") as n:
-        #     with open("nominees_2.txt", "w") as n2:
-        #         nominees = n.read()
-        #         for line in nominees.splitlines():
-        #             for winner in winners:
-        #                 if winner in line:
-        #                     nom = nlp(line)
-        #                     for ent in nom.ents:
-        #                         if ent.label_ == "PERSON":
-        #                             nominees_list[winner] = nominees_list.get(
-        #                                 winner, set(winner))
-        #                             nominees_list[winner].add(ent.text)
-        #                 break
-        #     json.dump(nominees_list, n2, indent=4)
+        final_awards = []
+        for k, v in final_winners.items():
+            max = 0
+            award = ""
+            category = ""
+            for k1, v1 in v:
+                for k2, v2 in v1:
+                    if v2 > max:
+                        max = v2
+                        award = k1
+                        category = k2
+            final_awards.append({
+                "award": award,
+                "category": category,
+                "winner": k
+            })
 
-        
-            
-
-            
+        print(json.dumps(final_awards, indent=4))
